a7a7e7af6361d409a0ca17c17eb08f113d37b14d
annieruth14 <ruthannie3@gmail.com>
2020-11-01 19:30:13 +0530

    [Annie]Added: test case to check if a person is added to the database as well as list

--- src/test/java/AddressBookDB/AddressBookTest.java
+++ src/test/java/AddressBookDB/AddressBookTest.java
@@ -15,12 +15,21 @@ public class AddressBookTest {
 		Assert.assertEquals(8, listOfAddress.size());
 	}
 	
+	// update data
 	@Test
 	public void givenNewAddress_whenUpdated_shouldSyncWithDatabase() throws AddressBookException {
 		ContactPerson contact = new ContactPerson();
 		List<AddressBook> listOfAddress = contact.getListFromDB();
 		contact.updateContact("Brian", "Siliguri");
 		boolean result = contact.checkInSync("Brian");
+		Assert.assertTrue(result);
+	}
+	
+	// add data to table and list
+	@Test
+	public void givenNewAddress_whenMultipleTables_shouldInsertInTheDatabase() throws AddressBookException {
+		ContactPerson contact = new ContactPerson();
+		boolean result = contact.addPersonInDB(6,"book6","batchmates","Neeraj","Ghosh",9874569362L,"james@gmail.com","Tripura","Agartala","258796");
 		Assert.assertTrue(result);
 	}
 }
