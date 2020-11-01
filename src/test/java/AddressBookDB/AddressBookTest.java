a46a549e755c918b9868edf0ef696a976b0c178b
annieruth14 <ruthannie3@gmail.com>
2020-11-01 15:38:19 +0530

    [Annie]Added: test case to check the number of entries in the AddressBook database

--- src/test/java/AddressBookDB/AddressBookTest.java
+++ src/test/java/AddressBookDB/AddressBookTest.java
@@ -1,5 +1,17 @@
 package AddressBookDB;
 
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class AddressBookTest {
 	
+	// check number of entries
+	@Test 
+	public void givenDatabase_shouldReturnCount() throws AddressBookException {
+		ContactPerson contact = new ContactPerson();
+		List<AddressBook> listOfAddress = contact.getCountFromDB();
+		Assert.assertEquals(8, listOfAddress.size());
+	}
 }
